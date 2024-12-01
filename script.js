d32ebfdb07947e7406e789c724e3e5e086f01f4c
Loges <kumarloges329@gmail.com>
2024-12-01 18:10:02 +0530

    Template Added

--- script.js
+++ script.js
@@ -6,22 +6,23 @@ const W_Speed = document.getElementById('W-Speed');
 const Cli = document.getElementById('Cli');
 const Hum = document.getElementById('Hum');
 const Vis = document.getElementById('Vis');
+const TDate = document.getElementById('Date');
 
-Btn.addEventListener('click', async () => {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=madurai&appid=bb5236fe0cf121cd0f443e8dbc78bbc1`, {
-        method: 'GET'
-    });
-    const data = await response.json();
-    console.log(data); 
-    Cname.textContent = data.name;
-    Temp.textContent = Math.round(data.main.temp - 273.15);
-    W_Speed.textContent = data.wind.speed;
-    Cli.textContent = data.weather[0].main;
-    Hum.textContent = data.main.humidity;
-    Vis.textContent = data.visibility / 1000;
-});
+// Btn.addEventListener('click', async () => {
+//     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=madurai&appid=bb5236fe0cf121cd0f443e8dbc78bbc1`, {
+//         method: 'GET'
+//     });
+//     const data = await response.json();
+//     console.log(data); 
+//     Cname.textContent = data.name;
+//     Temp.textContent = Math.round(data.main.temp - 273.15);
+//     W_Speed.textContent = data.wind.speed;
+//     Cli.textContent = data.weather[0].main;
+//     Hum.textContent = data.main.humidity;
+//     Vis.textContent = data.visibility / 1000;
+// });
 
-function getCurrentDateInfo() {
+const getCurrentDateInfo = () => {
     const today = new Date();
     const day = today.toLocaleString('en-US', { weekday: 'long' }).substring(0, 3);
     const month = today.getMonth();
@@ -37,7 +38,4 @@ function getCurrentDateInfo() {
 }
 
 const dateInfo = getCurrentDateInfo();
-console.log(`Day: ${dateInfo.day}`); 
-console.log(`Month: ${dateInfo.month}`); 
-console.log(`Date: ${dateInfo.date}`); 
-console.log(`Year: ${dateInfo.year}`); 
+TDate.textContent =  `${dateInfo.day} ${dateInfo.month} ${dateInfo.date} ${dateInfo.year}`;
